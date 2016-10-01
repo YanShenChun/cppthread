d9698ae42b8fcea082f4e31fbe5fd469a90e966c
emily <shanghaitan1987@126.com>
2016-10-01 21:55:15 +0800

    Remove the unneeded files

--- zthread/doc/bugs.js
+++
@@ -1,9 +0,0 @@
-// Fix for bug in IE6.0 float handling - from plone.org
-
-if (document.all && window.attachEvent) window.attachEvent("onload", fixWinIE);
-function fixWinIE() {
-    try {
-        document.getElementById('content').style.display = 'block';
-    } catch(er) {}
-}
-
